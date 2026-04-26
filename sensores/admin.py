82bc999a6946e71a710e577fd24ae61715a7fc1b
Daniele <danny.zancan8@gmail.com>
2026-04-26 11:42:58 -0300

    Registrando Motor no admin

--- sensores/admin.py
+++ sensores/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Motor  # Importe seu modelo aqui
 
-# Register your models here.
+@admin.register(Motor)
+class MotorAdmin(admin.ModelAdmin):
+    # Isso ajuda a ver as informações importantes direto na lista
+    list_display = ('nome', 'limite_alerta', 'ultimo_alerta_enviado')
